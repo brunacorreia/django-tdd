9d5a106a056574f678392631e63543a838678476
Bruna Moura Correia <mcorreia.bruna@gmail.com>
2021-11-18 18:45:31 -0300

    final tests

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -3,26 +3,36 @@ from selenium import webdriver
 from .forms import HashForm
 import hashlib
 from .models import Hash
+from django.core.exceptions import ValidationError
+import time
 
-# class FunctionalTestCase(TestCase):
+class FunctionalTestCase(TestCase):
 
-#     def setUp(self):
-#         self.browser = webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-#     def test_there_is_homepage(self):
-#         self.browser.get('http://localhost:8000')
-#         self.assertIn('Enter hash here:', self.browser.page_source)
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Enter hash here:', self.browser.page_source)
 
-#     def test_hash_of_hello(self):
-#         self.browser.get('http://localhost:8000')
-#         text = self.browser.find_element_by_id('id_text')
-#         text.send_keys('hello')
-#         self.browser.find_element_by_name('submit').click()
-#         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',self.browser.page_source)
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',self.browser.page_source)
+
+    def test_hash_ajax(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        time.sleep(5) # wait for AJAX
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',self.browser.page_source)
 
 
-#     def tearDown(self):
-#         self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
+
 
 class UniTestCase(TestCase):
     def test_home_homepage_template(self):
@@ -37,10 +47,27 @@ class UniTestCase(TestCase):
         text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
         self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash)
 
-    def test_hash_object(self):
+    def saveHash(self):
         hash = Hash()
         hash.text = 'hello'
         hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
         hash.save()
+        return hash
+
+
+    def test_hash_object(self):
+        hash = self.saveHash()
         pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
         self.assertEqual(hash.text,pulled_hash.text)
+
+    def test_viewing_hash(self):
+        hash = self.saveHash()
+        response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertContains(response, 'hello')
+
+    def test_bad_data(self):
+        def basHash():
+            hash = Hash()
+            hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824ggggggggg'
+            hash.full_clean()
+        self.assertRaises(ValidationError, basHash)
--- hashthat/hashthat/urls.py
+++ hashthat/hashthat/urls.py
@@ -21,4 +21,6 @@ from hashing import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('hash/<str:hash>', views.hash, name='hash'),
+    path('quickhash', views.quickhash, name='quickhash'),
 ]
